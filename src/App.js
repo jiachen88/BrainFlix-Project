55c41e0c67e4e7815a463838e7ab876de3665192
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-11-01 15:47:38 -0700

    Added views and likes
    added views and likes count for active video

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import logo from './assets/images/logo/BrainFlix-logo.svg';
 import imageMohan from './assets/images/Mohan-muruge.jpg'
 import videoData from './data/videos.json'
 import detailedVideoData from './data/video-details.json'
+import viewsIcon from './assets/images/icons/views.svg'
+import likesIcon from './assets/images/icons/likes.svg'
 
 
 function App() {
+  const [activeDetailedVideo, setActiveDetailedVideo] = useState(detailedVideoData[0])
   const [activeVideo, setActiveVideo] = useState(videoData[0]);
   const handleVideoClick = (id) => {
     const foundVideo = videoData.find(videoObject => videoObject.id === id);
     setActiveVideo(foundVideo);
-
   }
+
   return (
     <div>
       {/* brainflix header wrapper */}
@@ -25,15 +28,31 @@ function App() {
               placeholder='Search' />
           </form>
           <img className='brainflix-header__mohan' src={imageMohan} alt='Mohan profile' />
+          {/* Search Button Wrapped With Mohan Image */}
         </div>
         <button className='brainflix-header__button'>UPLOAD</button>
         <img className='brainflix-header__mohan-tablet' src={imageMohan} alt='Mohan profile' />
       </header >
+      {/* Video Section */}
       <section className='brainflix-video'>
         <video className='brainflix-video__content' controls poster={activeVideo.image}></video>
-        <h2>{activeVideo.title}</h2>
+        {/* Channel name, title, likes, and views */}
+        <div className='brainflix-video__details'>
+          <h2 className='brainflix-video__details-title'>{activeVideo.title}</h2>
+          <div className='brainflix-video__details-info'>
+            <div className='brainflix-video__details-publisher'>
+              <p>By {activeVideo.channel}</p>
+              <p>{activeDetailedVideo.timestamp}</p>
+            </div>
+            <div className='brainflix-video__details-stats'>
+              <p><img src={viewsIcon} alt='Views Icon' />{activeDetailedVideo.views}</p>
+              <p><img src={likesIcon} alt='Likes Icon' />{activeDetailedVideo.likes}</p>
+            </div>
+          </div>
+          <p>{activeDetailedVideo.description}</p>
+        </div>
       </section>
-    </div >
+    </div>
   );
 }
 
