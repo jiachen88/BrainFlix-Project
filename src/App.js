aa6bf0c56f80395e259b610684abd099b283f116
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-10-31 17:04:13 -0700

    Scss for header
    finished styling header for mobile view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import '../src/App.scss'
 import logo from './assets/images/logo/BrainFlix-logo.svg';
-import searchLogo from './assets/images/icons/search.svg'
 import imageMohan from './assets/images/Mohan-muruge.jpg'
-import uploadIcon from './assets/images/icons/upload.svg'
 function App() {
   return (
     <div>
+      {/* brainflix header wrapper */}
       <header className='brainflix-header'>
         <img className='brainflix-header__logo' src={logo} alt='logo' />
+        {/* Wrapping search bar with Mohan image */}
         <div className='brainflix-header__search'>
           <form className='brainflix-header__search-form'>
             <input className='brainflix-header__search-bar' type={'text'}
@@ -15,7 +15,8 @@ function App() {
           </form>
           <img className='brainflix-header__mohan' src={imageMohan} alt='Mohan profile' />
         </div>
-        <button className='brainflix-header__button'><img src={uploadIcon} alt='a icon for upload' />UPLOAD</button>
+        {/*  */}
+        <button className='brainflix-header__button'>UPLOAD</button>
       </header>
     </div >
   );
