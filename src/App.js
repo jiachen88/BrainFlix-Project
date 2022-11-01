8d0812d43281b55c5036637860db162aa27c04cb
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-11-01 14:41:52 -0700

    added active video component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import '../src/App.scss'
 import logo from './assets/images/logo/BrainFlix-logo.svg';
 import imageMohan from './assets/images/Mohan-muruge.jpg'
+import videoData from './data/videos.json'
+import detailedVideoData from './data/video-details.json'
+
+
 function App() {
+  const [activeVideo, setActiveVideo] = useState(videoData[0]);
+  const handleVideoClick = (id) => {
+    const foundVideo = videoData.find(videoObject => videoObject.id === id);
+    setActiveVideo(foundVideo);
+
+  }
   return (
     <div>
       {/* brainflix header wrapper */}
@@ -15,11 +26,13 @@ function App() {
           </form>
           <img className='brainflix-header__mohan' src={imageMohan} alt='Mohan profile' />
         </div>
-     
         <button className='brainflix-header__button'>UPLOAD</button>
         <img className='brainflix-header__mohan-tablet' src={imageMohan} alt='Mohan profile' />
-
       </header >
+      <section className='brainflix-video'>
+        <video className='brainflix-video__content' controls poster={activeVideo.image}></video>
+        <h2>{activeVideo.title}</h2>
+      </section>
     </div >
   );
 }
