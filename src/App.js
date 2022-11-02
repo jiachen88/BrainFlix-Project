cb135d78fee8e64c06f42517762070c780e4178c
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-11-01 17:20:45 -0700

    Comments Publishing Section
    updated scss for whole page and added comments section

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import likesIcon from './assets/images/icons/likes.svg'
 function App() {
   const [activeDetailedVideo, setActiveDetailedVideo] = useState(detailedVideoData[0])
   const [activeVideo, setActiveVideo] = useState(videoData[0]);
+  const activeDate = new Date(activeDetailedVideo.timestamp).toLocaleDateString();
+  console.log(activeDate)
   const handleVideoClick = (id) => {
     const foundVideo = videoData.find(videoObject => videoObject.id === id);
     setActiveVideo(foundVideo);
@@ -34,22 +36,39 @@ function App() {
         <img className='brainflix-header__mohan-tablet' src={imageMohan} alt='Mohan profile' />
       </header >
       {/* Video Section */}
-      <section className='brainflix-video'>
+      <main className='brainflix-video'>
         <video className='brainflix-video__content' controls poster={activeVideo.image}></video>
         {/* Channel name, title, likes, and views */}
         <div className='brainflix-video__details'>
           <h2 className='brainflix-video__details-title'>{activeVideo.title}</h2>
           <div className='brainflix-video__details-info'>
             <div className='brainflix-video__details-publisher'>
-              <p>By {activeVideo.channel}</p>
-              <p>{activeDetailedVideo.timestamp}</p>
+              <p className='brainflix-video__details-channel'>By {activeVideo.channel}</p>
+              <p className='brainflix-video__details-date'>{activeDate}</p>
             </div>
+            {/* Likes and Views */}
             <div className='brainflix-video__details-stats'>
-              <p><img src={viewsIcon} alt='Views Icon' />{activeDetailedVideo.views}</p>
-              <p><img src={likesIcon} alt='Likes Icon' />{activeDetailedVideo.likes}</p>
+              <p className='brainflix-video__details-views'><img src={viewsIcon} alt='Views Icon' />{activeDetailedVideo.views}</p>
+              <p className='brainflix-video__details-likes'><img src={likesIcon} alt='Likes Icon' />{activeDetailedVideo.likes}</p>
             </div>
           </div>
+          {/* Description of Video */}
           <p>{activeDetailedVideo.description}</p>
+
+        </div>
+      </main>
+      {/* Comments Section for Active Video */}
+      <section className='brainflix-comments'>
+        <h2 className='brainflix-comments__counter'>{activeDetailedVideo.comments.length} Comments</h2>
+        <div className='brainflix-comments__container'>
+          <img className='brainflix-comments__mohan' src={imageMohan} alt='Mohan Logo' />
+          <div className='brainflix-comments__container-medium'>
+            <div className='brainflix-comments__container-small'>
+              <p className='brainflix-comments__title'>Join The Conversation</p>
+              <textarea className='brainflix-comments__text'></textarea>
+            </div>
+            <button className='brainflix-comments__button'>COMMENT</button>
+          </div>
         </div>
       </section>
     </div>
