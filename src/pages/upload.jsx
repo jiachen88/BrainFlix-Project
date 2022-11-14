f455d055d95fc3d5e93aac8e3cd23e2f18f6deeb
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-11-14 00:25:17 -0800

    styling for upload page

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -3,23 +3,25 @@ import './upload.scss'
 function Upload() {
     return (<div className='upload-page'>
         <h2 className='upload-page__title'>Upload Video</h2>
-        <div className='upload-page__thumbnail'>
-            <p className='upload-page__thumbnail-title'>VIDEO THUMBNAIL</p>
-            <img className='upload-page__thumbnail-img' src={uploadImage} alt="Blue bicycle handles" />
-        </div>
-        <div>
-            <div>
-                <p>TITLE YOUR VIDEO</p>
-                <textarea></textarea>
+        <div className='upload-page__desktop'>
+            <div className='upload-page__thumbnail'>
+                <p className='upload-page__thumbnail-title'>VIDEO THUMBNAIL</p>
+                <img className='upload-page__thumbnail-img' src={uploadImage} alt="Blue bicycle handles" />
             </div>
-            <div>
-                <p>ADD A VIDEO DESCRIPTION</p>
-                <textarea></textarea>
+            <div className='video-inputs'>
+                <div className='video-inputs__title'>
+                    <p className='video-inputs__title-text'>TITLE YOUR VIDEO</p>
+                    <textarea placeholder='Add a title to your video' className='video-inputs__title-area'></textarea>
+                </div>
+                <div className='video-inputs__description'>
+                    <p className='video-inputs__description-text'>ADD A VIDEO DESCRIPTION</p>
+                    <textarea placeholder='Add a description to your video' className='video-inputs__description-area'></textarea>
+                </div>
             </div>
         </div>
-        <div>
-            <p>CANCEL</p>
-            <button>PUBLISH</button>
+        <div className='video-button'>
+            <button className='video-button__publish'>PUBLISH</button>
+            <p className='video-button__cancel'>CANCEL</p>
         </div>
     </div>
     )
