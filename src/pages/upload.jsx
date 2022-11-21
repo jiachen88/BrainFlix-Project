896a286ba26214de5f252c1626bf5542d3d1c5bd
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-11-21 01:40:04 -0800

    video upload

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -6,13 +6,15 @@ import './upload.scss'
 function Upload() {
     return (<div className='upload-page'>
         <h2 className='upload-page__title'>Upload Video</h2>
-        <div className='upload-page__desktop'>
-            <div className='upload-page__thumbnail'>
-                <p className='upload-page__thumbnail-title'>VIDEO THUMBNAIL</p>
-                <img className='upload-page__thumbnail-img' src={uploadImage} alt="Blue bicycle handles" />
-            </div>
-            <div className='video-inputs'>
-                <form id='videoUpload' action='' method='post'>
+        <form id='videoUpload' action='' method='post'>
+            <div className='upload-page__desktop'>
+                <div className='upload-page__thumbnail'>
+                    <p className='upload-page__thumbnail-title'>VIDEO THUMBNAIL</p>
+                    <img className='upload-page__thumbnail-img' src={uploadImage} alt="Blue bicycle handles" />
+                </div>
+
+                <div className='video-inputs'>
+
                     <div className='video-inputs__title'>
                         <p className='video-inputs__title-text'>TITLE YOUR VIDEO</p>
                         <textarea placeholder='Add a title to your video' className='video-inputs__title-area'></textarea>
@@ -21,13 +23,14 @@ function Upload() {
                         <p className='video-inputs__description-text'>ADD A VIDEO DESCRIPTION</p>
                         <textarea placeholder='Add a description to your video' className='video-inputs__description-area'></textarea>
                     </div>
-                </form>
+
+                </div>
             </div>
-        </div>
-        <div className='video-button'>
-            <Link to={'/videos/84e96018-4022-434e-80bf-000ce4cd12b8'}>  <button className='video-button__publish'>PUBLISH</button ></Link>
-            <p className='video-button__cancel'>CANCEL</p>
-        </div>
+            <div className='video-button'>
+                <Link to={'/videos/84e96018-4022-434e-80bf-000ce4cd12b8'}>  <button className='video-button__publish'>PUBLISH</button ></Link>
+                <p className='video-button__cancel'>CANCEL</p>
+            </div>
+        </form>
     </div >
     )
 }
