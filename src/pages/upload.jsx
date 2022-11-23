88c29cd2f85d4309e084830bcef727c8ca075e6d
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-11-22 17:13:36 -0800

    Update code

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -1,12 +1,21 @@
 import uploadImage from '../assets/images/Upload-video-preview.jpg'
-import { Link } from 'react-router-dom'
 import './upload.scss'
+import axios from 'axios'
 
 
 function Upload() {
+    function handleFormSubmit(event) {
+        event.preventDefault()
+        axios.post('http://localhost:5000/videos', {
+            title: event.target.title.value,
+            description: event.target.description.value
+        })
+        console.log(event.target.title.value)
+        console.log(event.target.description.value)
+    }
     return (<div className='upload-page'>
         <h2 className='upload-page__title'>Upload Video</h2>
-        <form id='videoUpload' action='' method='post'>
+        <form id='videoUpload' onSubmit={handleFormSubmit}>
             <div className='upload-page__desktop'>
                 <div className='upload-page__thumbnail'>
                     <p className='upload-page__thumbnail-title'>VIDEO THUMBNAIL</p>
@@ -17,20 +26,20 @@ function Upload() {
 
                     <div className='video-inputs__title'>
                         <p className='video-inputs__title-text'>TITLE YOUR VIDEO</p>
-                        <textarea placeholder='Add a title to your video' className='video-inputs__title-area'></textarea>
+                        <textarea placeholder='Add a title to your video' className='video-inputs__title-area' name='title'></textarea>
                     </div>
                     <div className='video-inputs__description'>
                         <p className='video-inputs__description-text'>ADD A VIDEO DESCRIPTION</p>
-                        <textarea placeholder='Add a description to your video' className='video-inputs__description-area'></textarea>
+                        <textarea placeholder='Add a description to your video' className='video-inputs__description-area' name='description'></textarea>
                     </div>
 
                 </div>
             </div>
             <div className='video-button'>
-                <Link to={'/videos/84e96018-4022-434e-80bf-000ce4cd12b8'}>  <button className='video-button__publish'>PUBLISH</button ></Link>
+                <button type='submit' className='video-button__publish'>PUBLISH</button >
                 <p className='video-button__cancel'>CANCEL</p>
             </div>
-        </form>
+        </form >
     </div >
     )
 }
